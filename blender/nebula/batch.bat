2f531fc3d1e648be74fff7dcce374a8adca4b973
Maciej Piaseczny <maciej.piaseczny@comarch.pl>
2022-03-06 23:35:29 +0100

    added blender files

---
+++ blender/nebula/batch.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Blender Foundation\Blender 3.0\blender.exe" -b 4.blend -o "C:\Users\Maciej Piaseczny\Downloads\priv\nebula\procedural\4.png" -f 6
+"C:\Program Files\Blender Foundation\Blender 3.0\blender.exe" -b 5.blend -o "C:\Users\Maciej Piaseczny\Downloads\priv\nebula\procedural\5.png" -f 6
+"C:\Program Files\Blender Foundation\Blender 3.0\blender.exe" -b 6.blend -o "C:\Users\Maciej Piaseczny\Downloads\priv\nebula\procedural\6.png" -f 6
+"C:\Program Files\Blender Foundation\Blender 3.0\blender.exe" -b 7.blend -o "C:\Users\Maciej Piaseczny\Downloads\priv\nebula\procedural\7.png" -f 6
